1cf03a3729a9717825e939f9543370494e07c0d1
snaking999 <65015065+snaking999@users.noreply.github.com>
2022-04-15 11:59:40 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,3 +13,4 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 sed -i 's/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/root:$1$aq6wq5ZZ$T1sQe0Mfn6.NZYRQ3S17..:18935:0:99999:7:::/g' package/lean/default-settings/files/zzz-default-settings
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
